d25f2aabd56bf52365d60491303205fb2782e681
ri0t <riot@c-base.org>
2019-03-09 21:04:22 +0100

    Isomer dependency version fixed to 1.0

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ An alerting system to notify system users.
 This software package is a plugin module for HFOS.
 """,
     dependency_links=[],
-    install_requires=['isomer>=1.0.0'],
+    install_requires=['isomer>=1.0'],
     entry_points="""[isomer.components]
     alert=isomer.alert.manager:Manager
 [isomer.schemata]
